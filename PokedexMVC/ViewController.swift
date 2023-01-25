266fa74501eec0b05cbd64e3261511b6dd6ffffa
2late4you <crash7333987@icloud.com>
2023-01-25 03:28:10 +0300

    Initial Commit

---
+++ PokedexMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokedexMVC
+//
+//  Created by Aliaksandr Sayenka on 25.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
