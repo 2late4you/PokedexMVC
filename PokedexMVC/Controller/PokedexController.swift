941fb79edf716b01c911d305bcf03e6e521d80f5
2late4you <crash7333987@icloud.com>
2023-02-13 02:08:14 +0300

    i

--- PokedexMVC/Controller/PokedexController.swift
+++ PokedexMVC/Controller/PokedexController.swift
@@ -5,4 +5,81 @@
 //  Created by Aliaksandr Sayenka on 25.01.23.
 //
 
-import Foundation
+import UIKit
+
+private let reuseIdentifier = "PokedexCell"
+
+class PokedexController: UICollectionViewController {
+    
+    
+    
+    // MARK: - Properies
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        configureViewComponents()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func showSearchBar() {
+        print(123)
+    }
+    
+    
+    // MARK: - Helper functions
+    
+    func configureViewComponents() {
+        collectionView.backgroundColor = .white
+        
+//        navigationController?.navigationBar.tintColor = .mainPink()
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isTranslucent = false
+        
+
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .mainPink()
+        appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        navigationItem.title = "Pokedex"
+
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showSearchBar))
+        
+        navigationItem.rightBarButtonItem?.tintColor = .white
+        
+        collectionView.register(PokedexCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
+
+extension PokedexController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PokedexCell
+        cell.backgroundColor = .blue
+        return cell
+    }
+}
+
+extension PokedexController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 32, left: 8, bottom: 8, right: 8)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - 36) / 3
+        return CGSize(width: width, height: width)
+    }
+}
